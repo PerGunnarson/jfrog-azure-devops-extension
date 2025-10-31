99c416355f181dfe433c47b182ba7fa123271d7c
PerGunnarson <56001835+PerGunnarson@users.noreply.github.com>
2025-10-31 11:45:06 +0100

    Added new async methods to utils.d.ts and added one missing non-async method to utils.js

--- jfrog-tasks-utils/utils.d.ts
+++ jfrog-tasks-utils/utils.d.ts
@@ -8,6 +8,12 @@ declare module '@jfrog/tasks-utils' {
         cliDownloadUrl?: string,
         cliAuthHandlers?: ifm.IRequestHandler[],
     ): void;
+    export async function executeCliTaskAsync(
+        runTaskFunc: (cliPath: string) => void,
+        cliVersion?: string,
+        cliDownloadUrl?: string,
+        cliAuthHandlers?: ifm.IRequestHandler[],
+    ): void;
     export function quote(str: string): string;
     export function downloadCli(cliDownloadUrl?: string, cliAuthHandlers?: ifm.IRequestHandler[], cliVersion?: string): Promise<string>;
     export function cliJoin(...args: string[]): string;
@@ -20,6 +26,7 @@ declare module '@jfrog/tasks-utils' {
     export function determineCliWorkDir(defaultPath: string, providedPath: string): string;
     export function compareVersions(version1: string, version2: string): number;
     export function executeCliCommand(cliCommand: string, runningDir: string, options?: {}): string;
+    export async function executeCliCommandAsync(cliCommand: string, runningDir: string, options?: {}): string;
     export function handleSpecFile(cliCommand: string, specPath: string): string;
     export function addBoolParam(cliCommand: string, inputParam: string, cliParam: string): string;
     export function addStringParam(cliCommand: string, inputParam: string, cliParam: string, require: boolean): string;
@@ -32,17 +39,31 @@ declare module '@jfrog/tasks-utils' {
         repoResolver: string,
         repoDeploy: string,
     ): string[];
+    export async function createBuildToolConfigFileAsync(
+        cliPath: string,
+        cmd: string,
+        requiredWorkDir: string,
+        configCommand: string,
+        repoResolver: string,
+        repoDeploy: string,
+    ): string[];
     export function addProjectOption(cliCommand: string): string;
     export function addServerIdOption(cliCommand: string, serverId: string): string;
     export function assembleUniqueServerId(usageType: string): string;
     export function configureDefaultXrayServer(usageType: string, cliPath: string, workDir: string): string;
+    export async function configureDefaultXrayServerAsync(usageType: string, cliPath: string, workDir: string): string;
     export function configureDefaultArtifactoryServer(usageType: string, cliPath: string, workDir: string): string;
+    export async function configureDefaultArtifactoryServerAsync(usageType: string, cliPath: string, workDir: string): string;
     export function configureDefaultDistributionServer(usageType: string, cliPath: string, workDir: string): string;
+    export async function configureDefaultDistributionServerAsync(usageType: string, cliPath: string, workDir: string): string;
     export function taskDefaultCleanup(cliPath: string, workDir: string, serverIdsArray: string[]): string;
+    export async function taskDefaultCleanupAsync(cliPath: string, workDir: string, serverIdsArray: string[]): string;
     export function appendBuildFlagsToCliCommand(cliCommand: string): string;
     export function isToolExists(tool: string): boolean;
     export function removeExtractorsDownloadVariables(cliPath: string, workDir: string);
+    export async function removeExtractorsDownloadVariablesAsync(cliPath: string, workDir: string);
     export function configureArtifactoryCliServer(artifactoryService: string, serverId: string, cliPath: string, buildDir: string);
+    export async function configureArtifactoryCliServerAsync(artifactoryService: string, serverId: string, cliPath: string, buildDir: string);
     export function setJdkHomeForJavaTasks();
     export function fetchAzureOidcToken(serviceConnectionID: string): string;
     export function exchangeOidcTokenAndSetStepVariables(
--- jfrog-tasks-utils/utils.js
+++ jfrog-tasks-utils/utils.js
@@ -101,6 +101,7 @@ module.exports = {
     configureArtifactoryCliServerAsync,
     configureDefaultArtifactoryServer,
     configureDefaultArtifactoryServerAsync,
+    configureDefaultDistributionServer,
     configureDefaultDistributionServerAsync,
     configureDefaultJfrogServer,
     configureDefaultJfrogServerAsync,
